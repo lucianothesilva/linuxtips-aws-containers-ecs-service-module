e4453433b13e7a8f6ec473f8d7e4be61203433ee
Luciano <lucianodefranca@outlook.com>
2024-09-14 14:43:46 -0300

    feat: start

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "service_name" {}
+
+variable "cluster_name" {}
+
+variable "vpc_id" {}
+
+variable "private_subnets" {}
+
+variable "service_port" {}
+
+variable "service_cpu" {}
+
+variable "service_memory" {}
+
+variable "service_listener" {}
+
+variable "service_task_execution_role" {}
