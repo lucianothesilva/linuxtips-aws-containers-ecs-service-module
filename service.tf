4b8e5725ecff11d19629f756ec9c9c546218d375
Luciano <lucianodefranca@outlook.com>
2024-09-29 19:07:25 -0300

    feat: change placement strategy

--- service.tf
+++ service.tf
@@ -14,10 +14,19 @@ resource "aws_ecs_service" "main" {
     rollback = true
   }
 
-ordered_placement_strategy {
-  type = "spread"
-  field = "attribute:ecs.availability-zone"
+dynamic "ordered_placement_strategy" {
+  for_each = var.service_launch_type == "EC2" ? [1] : []
+  content {
+    type = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
 }
+
+
+#ordered_placement_strategy {
+#  type = "spread"
+#  field = "attribute:ecs.availability-zone"
+#}
 
   network_configuration {
     security_groups  = [aws_security_group.main.id]
